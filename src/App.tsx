c41f7d1b326ebdb5bb1dfe112b263ded8f0438cb
Dev <dev@harryadney.com>
2025-11-07 15:59:41 +0000

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,13 @@ import { Editor } from './pages/Editor';
 import { Admin } from './pages/Admin';
 import { PuckPage } from './pages/PuckPage';
 import { PageManager } from './pages/PageManager';
+import { Home } from './pages/Home';
+import { BishopdaleValley } from './pages/BishopdaleValley';
+import { FourTownships } from './pages/FourTownships';
+import { Timeline } from './pages/Timeline';
+import { Maps } from './pages/Maps';
+import { Gallery } from './pages/Gallery';
+import { Contact } from './pages/Contact';
 
 function App() {
   return (
@@ -14,13 +21,13 @@ function App() {
         <div className="min-h-screen flex flex-col bg-texture">
           <Header />
           <Routes>
-            <Route path="/" element={<PuckPage slug="home" />} />
-            <Route path="/bishopdale-valley" element={<PuckPage slug="bishopdale-valley" />} />
-            <Route path="/four-townships" element={<PuckPage slug="four-townships" />} />
-            <Route path="/timeline" element={<PuckPage slug="timeline" />} />
-            <Route path="/maps" element={<PuckPage slug="maps" />} />
-            <Route path="/gallery" element={<PuckPage slug="gallery" />} />
-            <Route path="/contact" element={<PuckPage slug="contact" />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/bishopdale-valley" element={<BishopdaleValley />} />
+            <Route path="/four-townships" element={<FourTownships />} />
+            <Route path="/timeline" element={<Timeline />} />
+            <Route path="/maps" element={<Maps />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/contact" element={<Contact />} />
             <Route path="/pages" element={<PageManager />} />
             <Route path="/editor/:slug" element={<Editor />} />
             <Route path="/editor" element={<Editor />} />
